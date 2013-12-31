d5008d7cb8f1270b38e9ae80c2eaded5a0c1ae4b
Subeesh <subeeshb@gmail.com>
2013-12-31 10:44:08 +0800

    Reorganized folder structure

---
+++ sample-app/test/utils/componentTestingUtils.js
@@ -0,0 +1,80 @@
+var ComponentTestingUtils = {
+
+	App: {},
+	container: null,
+
+	setup: function() {
+		$('body').append('<div id="test-fixture"></div>');
+		Ember.TEMPLATES.application = Ember.Handlebars.compile("<div>{{outlet componentContainer}}</div>");
+	},
+
+	cleanup: function() {
+		Ember.run(function() {
+		    App.destroy();
+		    App = null;
+	  	});
+	},
+
+	getComponentName: function(componentController) {
+		return componentController.toString().split('.').slice(-1);
+	},
+
+	setupComponentForTesting: function(componentTemplateName, componentController, callback) {
+		
+		var componentName = this.getComponentName(componentController);
+		
+		Ember.TEMPLATES.componentContainer = Ember.Handlebars.compile("{{"+componentTemplateName+"}}");
+
+		Ember.run(function() {
+	    App = Ember.Application.create({
+	      name: 'App',
+	      rootElement: '#test-fixture'
+	    });
+
+	    App[componentName]= componentController.extend({});
+
+	    App.IndexRoute = Ember.Route.extend({
+
+		    setupController: function (controller) {
+		    },
+
+		    renderTemplate: function () {
+		    	console.log('render');
+		        this.render('componentContainer', {
+		            outlet: 'componentContainer'
+		        });
+
+		    }
+		});
+
+		App.IndexController = Ember.ObjectController.extend({
+		});
+
+
+	    App.deferReadiness();
+
+	    App.Router = Ember.Router.extend({
+	      location: 'none'
+	    });
+
+	    container = App.__container__;
+
+	    container.register('component:'+componentTemplateName, App[componentName]);
+
+	    
+	  });
+
+
+	  
+
+	  var router = container.lookup('router:main');
+
+	  Ember.run(App, 'advanceReadiness');
+	  Ember.run(function() {
+	    router.handleURL('/');
+
+	    if (callback) { callback(); }
+	  });
+	}
+
+};
